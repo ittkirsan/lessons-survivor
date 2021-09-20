c4068f066c2b140cb0d677ba6576ee323d6c94b2
Dmitrievich Vyacheslav <ittkirsan@gmail.com>
2021-09-20 16:20:18 +0300

    Create task4

---
+++ lesson4.py
@@ -0,0 +1,15 @@
+
+
+def MadMax(N, Tele):
+    Tele.sort()
+    Tele2 = []
+
+    for i in range(N-1):
+        if i <= (N-1)/2:
+            Tele2.append(Tele[i])
+
+    for i in range(N-1, -1, -1):
+        if i > (N-1)/2:
+            Tele2.append(Tele[i])
+
+    return Tele2

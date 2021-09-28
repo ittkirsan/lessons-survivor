fe1c0dbc1bcf1e0497839bb21892ec0ffd41c766
Dmitrievich Vyacheslav <ittkirsan@gmail.com>
2021-09-28 12:36:40 +0300

    Update task 7 - WordSearch

---
+++ lesson7_1.py
@@ -0,0 +1,48 @@
+
+def WordSearch(N, s, subs):
+    str1 = ''
+    result = []
+    result_list = []
+
+    l = list(s)
+
+    while len(l) > N:
+        count = 0
+        for i in range(N, 0, -1):
+            if l[i] == ' ':
+                k = i
+                count = 1
+                for j in range(0, k+1):
+                    str1 = str1 + l[0]
+                    l.pop(0)
+
+                result.append(str1)
+
+                str1 = ''
+                break
+        if count == 0:
+            for j in range(0, N):
+                str1 = str1 + l[0]
+                l.pop(0)
+            result.append(str1)
+
+            str1 = ''
+
+    for m in range(len(l)):
+        str1 = str1 + l[0]
+        l.pop(0)
+
+    result.append(str1)
+
+    for strings in result:
+        mark = 0
+        for words in strings.split():
+            if words == subs:
+                mark = 1
+        if mark == 1:
+            result_list.append(1)
+
+        else:
+            result_list.append(0)
+
+    return result_list

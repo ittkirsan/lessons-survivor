c4ee5273816a6964cb7ac03b16fbecf664164684
Dmitrievich Vyacheslav <ittkirsan@gmail.com>
2021-10-22 22:00:22 +0300

    Added test for lesson15

---
+++ lesson15_test.py
@@ -0,0 +1,22 @@
+import unittest
+from lesson15 import TankRush
+
+
+class func_test(unittest.TestCase):
+    def TankRansh(self):
+        self.assertEqual(TankRush(3, 4, "1234 2345 0987", 2, 2, "34 98"), True)
+        self.assertEqual(
+            TankRush(3, 4, "1234 5674 0987", 2, 2, "34 98"), False)
+        self.assertEqual(TankRush(3, 4, "1234 4567 7890", 2, 2, "34 67"), True)
+        self.assertEqual(
+            TankRush(3, 4, "1234 5647 8901", 2, 2, "12 01"), False)
+        self.assertEqual(
+            TankRush(4, 6, '578914 123456 045782 458792', 3, 3, '914 222 782'), True)
+        self.assertEqual(
+            TankRush(4, 6, '578914 123456 045782 458792', 3, 3, '914 456 782'), False)
+        self.assertEqual(
+            TankRush(4, 6, '578914 123456 045782 458792', 2, 2, '87 92'), False)
+
+
+if __name__ == '__main__':
+    unittest.main()

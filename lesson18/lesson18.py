acac7b67a2bae105c49ac91e7d26479e33c7c1ab
Dmitrievich Vyacheslav <ittkirsan@gmail.com>
2021-10-30 21:29:39 +0300

    Added task18 - MisterRobot

---
+++ lesson18/lesson18.py
@@ -0,0 +1,22 @@
+
+
+def MisterRobot(N: int, data: int):
+    etalon = sorted(data)
+    for k in range(N-1):
+        for i in range(N, 2, -1):
+            if data[i-1] == data and data[i-3] < data[i-2][i-2] + 1 and data[i-3] == data[i-2] + 1:
+                break
+            elif data[i-3] < data[i-2] and data[i-3] < data[i-1]:
+                continue
+
+            else:
+                for j in range(3):
+                    if (data[i-3] > data[i-2]) or (data[i-3] > data[i-1]):
+                        data[i-1], data[i-2], data[i -
+                                                   3] = data[i-3], data[i-1], data[i-2]
+                    else:
+                        break
+    if data == etalon:
+        return True
+    else:
+        return False

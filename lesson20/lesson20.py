ce20e74721ae924426c960f50991ad0b9ae961a9
Dmitrievich Vyacheslav <ittkirsan@gmail.com>
2021-11-09 15:31:21 +0300

    Update task20 and new test

--- lesson20/lesson20.py
+++ lesson20/lesson20.py
@@ -1,10 +1,10 @@
 list_string = []  # список строк для операций 1 и 2
 undo = []  # список для отмененых операций
-flag = True
+flag = 0
 
 
 def BastShoe(command):
-    global list_string, undo, flag
+    global flag, list_string, undo
 
     # разбиваем входящую строку на номер команды и аргументы
     command = command.split(' ', maxsplit=1)
@@ -12,31 +12,34 @@ def BastShoe(command):
     if command[0] == '1':
         if len(undo) > 0:  # для прекращения цепочки для операции 4
             undo.clear()
-            flag = False
-        elif len(undo) == 0:
-            flag = True
+            del list_string[:-1]
+            flag = -1
         if len(list_string) == 0:
             list_string.append(command[1])
+            flag += 1
             return list_string[0]
         else:
             list_string.append(list_string[-1] + command[1])
-        return list_string[len(list_string)-1]
+            flag += 1
+            return list_string[len(list_string)-1]
 
     # выполняем операцию 2
     elif command[0] == '2':
         if len(undo) > 0:
             undo.clear()
-            flag = False
-        elif len(undo) == 0:
-            flag = True
-        if len(list_string) == 0:
-            return ''
-        elif len(list_string[-1]) >= int(command[1]):
+            list_string.append('')
+            del list_string[:-1]
+            flag = -1
+        else:
+            undo.append('')
+
+        if len(list_string[-1]) >= int(command[1]):
             list_string.append(list_string[-1][:-int(command[1])])
+            flag += 1
             return list_string[-1]
         else:
-            list_string.append('')
-            return list_string[-1]
+            flag += 1
+            return ''
 
     # выполняем операцию 3
     elif command[0] == '3':
@@ -47,24 +50,20 @@ def BastShoe(command):
 
     # выполняем операцию 4
     elif command[0] == '4':
-        if flag == False:
-            if len(undo) == 0:
-                undo.append(list_string[-1])
-                list_string.pop()
-                return list_string[-1]
-            else:
-                return list_string[-1]
-        elif len(list_string) > 0:
+        if len(list_string) == 0:
+            return ""
+        elif flag > 0:
             undo.append(list_string[-1])
             list_string.pop()
+            flag -= 1
             if len(list_string) == 0:
                 return ""
             else:
                 return list_string[-1]
-        else:
-            return ""
+        elif flag <= 0:
+            return list_string[0]
 
-    # выполняем операцию 5
+            # выполняем операцию 5
     elif command[0] == "5":
         if len(undo) == 0:
             return list_string[-1]
